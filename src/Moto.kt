be2672862b3e8345f0df3e78c0e399e0fec73c76
Catalina Cavallo <catalinacavallo10@gmail.com>
2024-10-15 22:28:14 -0300

    Revert "Feat/moto"

--- src/Moto.kt
+++
@@ -1,7 +0,0 @@
-class Moto (
-    nombre: String,
-    velocidadMaxima: Int=180,
-    combustible: Int=60,
-    aceleracion: Int=20,
-    frenado: Int=15,
-    ): VehiculoCustom (nombre, velocidadMaxima, combustible, aceleracion, frenado)

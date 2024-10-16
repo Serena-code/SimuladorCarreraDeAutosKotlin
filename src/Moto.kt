74338239f1b58705a70bdc21287c5a6cbecc60bc
Catalina Cavallo <catalinacavallo10@gmail.com>
2024-10-15 22:20:54 -0300

    Feat Moto agregada

---
+++ src/Moto.kt
@@ -0,0 +1,7 @@
+class Moto (
+    nombre: String,
+    velocidadMaxima: Int=180,
+    combustible: Int=60,
+    aceleracion: Int=20,
+    frenado: Int=15,
+    ): VehiculoCustom (nombre, velocidadMaxima, combustible, aceleracion, frenado)

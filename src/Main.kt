87144b1792c0ff7c1831cc87d73426c297a1de98
Serena Ludueña <sereluduena@gmail.com>
2024-10-22 22:11:01 -0300

    a

--- src/Main.kt
+++ src/Main.kt
@@ -13,12 +13,47 @@ fun main() {
     camion.avanzar()
     camion.acelerar()
     camion.frenar()
-   println(camion.toString())
+    println(camion.toString())
 
     autodeportivo.avanzar()
     autodeportivo.acelerar()
     autodeportivo.frenar()
     println(autodeportivo.toString())
+
+    val pista = Pista(100, "soleado")
+    //TODO instanciar Pista Custom
+    //TODO Instanciar los distintos vehiculos
+    val vehiculos: List<Vehiculo> = listOf()
+
+    var ganador: Vehiculo? = null
+    var turnos = 0
+
+    while (ganador == null) {
+        turnos++
+
+        println("----- Turno $turnos -----\n")
+
+        // Cambiar clima en cada 3 turnos
+        if (turnos % 3 == 0) {
+            pista.cambiarClima()
+        }
+
+        // Simulación de la carrera
+        vehiculos.forEach { vehiculo ->
+            //TODO: Completar metodos
+
+            if (vehiculo.distanciaRecorrida >= pista.longitud) {
+                ganador = vehiculo
+            }
+            println(vehiculo.toString())
+        }
+
+        println("----------------\n")
+        // Pausa entre turnos
+        Thread.sleep(1000)
+    }
+
+    println("🏁 ${ganador?.nombre} ha ganado la carrera en $turnos turnos!")
 }
 
 
